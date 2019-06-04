6cad11809a52dd2b3dd6103687c195caaeebceaf
Muzorn <jesusmgc.it@gmail.com>
2019-06-04 19:06:17 +0200

    Creamos un componente llamado "Listado" y añadimos su etiqueta a la plantilla principal.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { ListadoComponent } from './listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    HeroesComponent
+    HeroesComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule,
