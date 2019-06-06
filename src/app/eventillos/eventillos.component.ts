ee18d91523b4bc05a85a6ca7ff7b596884916f36
Muzorn <jesusmgc.it@gmail.com>
2019-06-06 12:32:33 +0200

    Creamos un nuevo componente "Eventillos" y añadimos la etiqueta correspondiente a la plantilla principal.

---
+++ src/app/eventillos/eventillos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventillos',
+  templateUrl: './eventillos.component.html',
+  styleUrls: ['./eventillos.component.css']
+})
+export class EventillosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
