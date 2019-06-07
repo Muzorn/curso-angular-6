96b158ab117cbf03f81df06c975f8e397933dea9
Muzorn <jesusmgc.it@gmail.com>
2019-06-07 14:02:26 +0200

    Corregimos los problemas de definición de la constante de rutas (faltaba un puñetero =) y las importamos correctamente para que carguen. Definimos varias rutas a varios de nuestros componentes creados a lo largo del curso.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,17 @@ import { ResultadosComponent } from './resultados/resultados.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ResultadosServiceService} from './resultados-service.service';
 import { EventillosComponent } from './eventillos/eventillos.component';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
 
-const routes: Routes [
-  { path: 'home', component: HomeComponentComponent }
+const routes: Routes = [
+  { path: 'home', component: HomeComponentComponent },
+  { path: 'todo', component: TodosComponent },
+  { path: 'clientes', component: MisclientesComponent },
+  { path: 'eventos', component: EventillosComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'resultados-electorales', component: ResultadosComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
   ];
 
 @NgModule({
@@ -37,7 +43,9 @@ const routes: Routes [
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    RouterModule,
+    RouterModule.forRoot(
+      routes
+    ),
   ],
   providers: [TodoService, ResultadosServiceService],
   bootstrap: [AppComponent]
